560adf33a517c1dad9f41382f8fe3826033d7503
干净 <54979375+du5@users.noreply.github.com>
2021-06-24 20:33:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# SSPanel-Uim-Docker
---
+++ build.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+docker build ./file/php -t myphp
+
+rm -rf SSPanel-Uim/vendor SSPanel-Uim/composer.lock 
+
+docker run --rm -v $PWD/SSPanel-Uim:/app composer composer install  --ignore-platform-reqs
+
+docker-compose up -d
+
+sleep 20s 
+# 创建数据库
+docker exec -i mariadb sh -c 'exec mysql -uroot -p"$MARIADB_ROOT_PASSWORD" -e"CREATE DATABASE sspanel CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"'
+docker exec -i mariadb sh -c 'exec mysql -uroot -p"$MARIADB_ROOT_PASSWORD" sspanel' < ./SSPanel-Uim/sql/glzjin_all.sql
+
+# 复制配置文件
+cp SSPanel-Uim/config/.config.example.php SSPanel-Uim/config/.config.php
+cp SSPanel-Uim/config/appprofile.example.php SSPanel-Uim/config/appprofile.php
+
+# 设置 php 权限
+docker exec -i php sh -c 'exec chmod -R 755 `pwd`'
+docker exec -i php sh -c 'exec chown -R www-data:www-data `pwd`'
+
+docker exec -i php sh -c 'exec php xcat User createAdmin'
+docker exec -i php sh -c 'exec php xcat Tool initQQWry'
+docker exec -i php sh -c 'exec php xcat ClientDownload'
+
+
