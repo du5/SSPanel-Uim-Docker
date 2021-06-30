c1761650ed2b4d0cae2b95e1a413fc26f976f9ca
干净 <54979375+du5@users.noreply.github.com>
2021-06-30 15:36:24 +0800

    支持时区设置

--- file/php/push.bash
+++ file/php/push.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+docker pull  php:7.4-fpm-alpine3.13
 docker buildx build --platform linux/ppc64le,linux/arm64/v8,linux/amd64,linux/s390x,linux/arm/v7,linux/arm/v6,linux/386 ./file/php --push  -t gtary/php:ssp
