5b7c37fee84ad0b696780ca6338ebd507acc4ed3
干净 <54979375+du5@users.noreply.github.com>
2021-07-20 11:38:24 +0800

    修正PHP容器ZipArchive函数缺失

--- file/php/push.bash
+++ file/php/push.bash
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker pull  php:7.4-fpm-alpine3.13
-docker buildx build --platform linux/ppc64le,linux/arm64/v8,linux/amd64,linux/s390x,linux/arm/v7,linux/arm/v6,linux/386 ./file/php --push  -t gtary/php:ssp
+docker pull php:7.4-fpm-alpine3.13
+docker pull nginx:alpine
+
+docker buildx build --platform linux/ppc64le,linux/arm64/v8,linux/amd64,linux/s390x,linux/arm/v7,linux/arm/v6,linux/386 ./file/php --push  -t gtary/ssp:php
+docker buildx build --platform linux/ppc64le,linux/arm64/v8,linux/amd64,linux/s390x,linux/arm/v7,linux/arm/v6,linux/386 . --push -t gtary/ssp:nginx
