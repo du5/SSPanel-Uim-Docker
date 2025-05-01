d28329c15ce6a53663b7f9614ce20bd19c77399b
干净 <54979375+du5@users.noreply.github.com>
2025-05-01 20:21:08 +0800

    更新 PHP 版本至 8.3，调整 Dockerfile 和推送脚本，修正 MariaDB 镜像及路径配置

--- file/php/push.bash
+++ file/php/push.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker pull php:7.4-fpm-alpine3.13
+docker pull php:8.3-fpm-alpine
 docker pull nginx:alpine
 
 docker buildx build --platform linux/ppc64le,linux/arm64/v8,linux/amd64,linux/s390x,linux/arm/v7,linux/arm/v6,linux/386 ./file/php --push  -t gtary/ssp:php
