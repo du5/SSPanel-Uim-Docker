86fdb458c08fb2615258e812d07525a646dfd538
干净 <54979375+du5@users.noreply.github.com>
2025-05-10 02:23:45 +0800

    重构 nginx 配置，简化 server 块并移除冗余配置

--- file/php/push.bash
+++ file/php/push.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull php:8.3-fpm-alpine
 
-docker buildx build --platform linux/ppc64le,linux/arm64/v8,linux/amd64,linux/s390x,linux/arm/v7,linux/arm/v6,linux/386 . --push  -t gtary/ssp:php
+docker buildx build --platform linux/ppc64le,linux/arm64/v8,linux/amd64,linux/s390x,linux/arm/v7,linux/arm/v6,linux/386 . --push -t gtary/ssp:php
